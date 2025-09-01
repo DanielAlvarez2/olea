d734f91567f24acb193550a18b979ed7af2a79fb
Daniel Alvarez <daniel.yllanes@hotmail.com>
2025-09-01 15:30:47 -0400

    re-add strictmode import

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,4 +1,5 @@
 import {BrowserRouter, Routes,Route} from 'react-router'
+import { StrictMode } from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import Test from './Test.jsx'
