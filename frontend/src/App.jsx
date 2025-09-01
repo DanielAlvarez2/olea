3b4842a37c8fa90c5d8e63ef4090ba196a4f2303
Daniel Alvarez <daniel.yllanes@hotmail.com>
2025-09-01 13:52:31 -0400

    add BASE_URL from Render.com in App.jsx fetch()

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,10 +1,14 @@
 import { useState,useEffect } from 'react'
 
 export default function App() {
+  const BASE_URL = (process.env.NODE_ENV == 'production') ? 
+                    'https://olea-iwpz.onrender.com/' :
+                    'http://localhost:1436'
+
   const [specials, setSpecials] = useState([])
 
   const getSpecials = ()=>{
-    fetch('/api/special')
+    fetch(`${BASE_URL}/api/special`)
       .then(res=>res.json())
       .then(json=>setSpecials(json))
       .catch(err=>console.log(err))
