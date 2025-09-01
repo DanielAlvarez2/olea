1cfbdc208df2a9743f385adbd36044898ce953ad
Daniel Alvarez <daniel.yllanes@hotmail.com>
2025-09-01 16:18:10 -0400

    bug fix: had an extra <App /> component at the bottom of main.jsx

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -12,6 +12,5 @@ ReactDOM.createRoot(document.getElementById('root')).render(
         <Route path='test' element={<Test />} />
       </Routes>
     </BrowserRouter>
-    <App />
-  </StrictMode>,
+  </StrictMode>
 )
