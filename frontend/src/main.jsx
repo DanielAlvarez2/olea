b647de6252110263af12c7f2d8833c6aced301db
Daniel Alvarez <daniel.yllanes@hotmail.com>
2025-09-01 15:28:39 -0400

    remove unused imports inm main.jsx

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,9 +1,5 @@
-import React from 'react'
 import {BrowserRouter, Routes,Route} from 'react-router'
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
 import ReactDOM from 'react-dom/client'
-import './index.css'
 import App from './App.jsx'
 import Test from './Test.jsx'
 
