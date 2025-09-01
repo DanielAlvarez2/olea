85ae301c977c379860272ac4753a9759fe0c32fe
Daniel Alvarez <daniel.yllanes@hotmail.com>
2025-09-01 15:17:03 -0400

    install and implememt react-router

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,10 +1,20 @@
+import React from 'react'
+import {BrowserRouter, Routes,Route} from 'react-router'
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import ReactDOM from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
+import Test from './Test.jsx'
 
-createRoot(document.getElementById('root')).render(
+ReactDOM.createRoot(document.getElementById('root')).render(
   <StrictMode>
+    <BrowserRouter>
+      <Routes>
+        <Route path='/' element={<App />} />
+        <Route path='test' element={<Test />} />
+      </Routes>
+    </BrowserRouter>
     <App />
   </StrictMode>,
 )
