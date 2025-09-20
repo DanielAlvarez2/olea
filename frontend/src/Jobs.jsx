ae5742410826ed824ecb1e143cc47e060211f870
Daniel Alvarez <daniel.yllanes@hotmail.com>
2025-09-20 01:28:20 -0400

    add input fields in Jobs.jsx

--- frontend/src/Jobs.jsx
+++ frontend/src/Jobs.jsx
@@ -17,7 +17,7 @@ export default function Jobs(){
                             <div>APPLICATION FOR EMPLOYMENT</div>
                             <div>OLEA RESTAURANT</div>
                             <div>39 High Street</div>
-                            <div>New Haven, CT 06510</div>
+                            <div>New Haven, CT 06510</div><br/>
                             <div style={{border:'1px solid white',padding:'3px'}}>Olea is an equal opportunity employer and does not discriminate against
                                 otherwise qualified applicants on the basis of race, color, creed, religion,
                                 ancestry, age, sex, marital status, national origin, disability or handicap, or
@@ -41,22 +41,156 @@ export default function Jobs(){
                                 </div>{/* .name-flexbox */}
                                 
                                 <br/>
+                                
                                 <label>
                                     Street Address:<br/>
                                     <input type='text' />
                                 </label><br/>
+                                
                                 <label>
                                     City:<br/>
                                     <input type='text' />
                                 </label><br/>
+                                
                                 <label>
                                     State:<br/>
                                     <input type='text' />
                                 </label><br/>
+                                
                                 <label>
                                     Zip Code:<br/>
                                     <input type='text' />
+                                </label><br/>
+                                
+                                <label>
+                                    Position Sought:<br/>
+                                    <input type='text' />
+                                </label><br/>
+                                
+                                <label>
+                                    Availability:<br/>
+                                    <select>
+                                        <option>Select One...</option>
+                                        <option>Part-Time</option>
+                                        <option>Full-Time</option>
+                                    </select>
+                                </label><br/><br/>
+
+                                <label>
+                                    Date Available:<br/>
+                                    <input type='text' />
+                                </label><br/>
+
+                                <label>
+                                    Salary Desired:<br/>
+                                    <input type='text' />
+                                </label><br/>
+
+                                <label>
+                                    Phone Number:<br/>
+                                    <input type='text' />
+                                </label><br/>
+
+                                <label>
+                                    Email Address:<br/>
+                                    <input type='email' />
+                                </label><br/><br/>
+
+                                <label>
+                                    Are you over 18 years old?<br/>
+                                    <input type='radio' name='adult' required /> Yes<br/>
+                                    <input type='radio' name='adult' /> No
+                                </label><br/><br/>
+
+                                <label>
+                                    Are you legally eligible for employment in the United States? <br/>
+                                    <input type='radio' name='legal' required /> Yes<br/>
+                                    <input type='radio' name='legal' /> No
                                 </label>
+
+                                <br/><br/>
+                                <hr/>
+                                <br/>
+                                EDUCATION:<br/>
+                                Please indicate education or training which you believe qualifies you for the position you are seeking.<br/>
+                                <br/>
+                                <label>
+                                HIGH SCHOOL<br/>
+                                <div className='high-school'>
+                                    <div className='years'>
+                                        <div>1</div>
+                                        <div><input type='radio' name='hs-years' /></div>
+                                    </div>
+                                    <div className='years'>
+                                        <div>2</div>
+                                        <div><input type='radio' name='hs-years' /></div>
+                                    </div>
+                                    <div className='years'>
+                                        <div>3</div>
+                                        <div><input type='radio' name='hs-years' /></div>
+                                    </div>
+                                    <div className='years'>
+                                        <div>4</div>
+                                        <div><input type='radio' name='hs-years' /></div>
+                                    </div>
+                                </div>
+                                </label>
+                                Number of years completed<br/><br/>
+                                <label>
+                                    High School Diploma?<br/>
+                                    <input type='radio' name='hs-diploma' /> Yes<br/>
+                                    <input type='radio' name='hs-diploma' /> No                                    
+                                </label><br/><br/>
+
+                                <label>
+                                    G.E.D.?<br/>
+                                    <input type='radio' name='GED' /> Yes<br/>
+                                    <input type='radio' name='GED' /> No
+                                </label><br/><br/>
+
+                                <label>
+                                    Name of School:<br/>
+                                    <input type='text' />
+                                </label><br/>
+
+                                <label>
+                                    City/State:<br/>
+                                    <input type='text' />
+                                </label>
+                                
+                                <br/><br/><br/><br/>
+
+                                <label>
+                                    COLLEGE and/or VOCATIONAL SCHOOL
+                                    <div className='college'>
+                                        <div className='years'>
+                                            <div>1</div>
+                                            <div><input type='radio' name='college-years' /></div>
+                                        </div>
+                                        <div className='years'>
+                                            <div>2</div>
+                                            <div><input type='radio' name='college-years' /></div>
+                                        </div>
+                                        <div className='years'>
+                                            <div>3</div>
+                                            <div><input type='radio' name='college-years' /></div>
+                                        </div>
+                                        <div className='years'>
+                                            <div>4</div>
+                                            <div><input type='radio' name='college-years' /></div>
+                                        </div>
+                                    </div>
+                                </label>
+                                Number of years completed<br/><br/>
+                                <label>
+                                    Name of School:<br/>
+                                    <input type='text' />
+                                </label><br/>
+
+                                <label>
+                                    City/State:<br/>
+                                    <input type='text' />
+                                </label><br/><br/>
 
                                 
                             </form>
