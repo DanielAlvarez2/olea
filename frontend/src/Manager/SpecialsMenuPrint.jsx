d785262a26807a5a8a36e41a0907ca01dafe5760
Daniel Alvarez <daniel.yllanes@hotmail.com>
2025-12-24 13:25:45 -0500

    .main-menu padding-bottom:0 in SpecialsMenuPrint.jsx

--- frontend/src/Manager/SpecialsMenuPrint.jsx
+++ frontend/src/Manager/SpecialsMenuPrint.jsx
@@ -106,7 +106,8 @@ Please switch to a different browser to proceed.
                                 // justifyContent:'space-evenly',
                                 alignItems:'center',
                                 height:'100%',
-                                border:'1px solid green'
+                                border:'1px solid green',
+                                paddingBottom:'0'
                                 }}>
 
 
