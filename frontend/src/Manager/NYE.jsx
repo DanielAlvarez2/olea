a3196b1e5115e175b3e6f387e22b137a8ae8a77e
Daniel Alvarez <daniel.yllanes@hotmail.com>
2026-01-03 23:42:05 -0500

    create NYE.jsx

---
+++ frontend/src/Manager/NYE.jsx
@@ -0,0 +1,31 @@
+import {Link} from 'react-router'
+import './Manager.css'
+import ManagerNavbar from './components/ManagerNavbar.jsx'
+
+export default function NYE(){
+    return(
+        <>
+            <div className='manager-page-wrapper' style={{border:'1px solid red'}}>
+                <ManagerNavbar page='events' />
+                    <div style={{textAlign:'center',fontSize:'30px'}}>menu manager</div>
+                    <div style={{textAlign:'center',fontSize:'30px'}}>events &gt; NYE</div>
+                    <ul className='main-menu' 
+                        style={{display:'flex',
+                                flex:'1',
+                                flexDirection:'column',
+                                gap:'10px',
+                                justifyContent:'space-evenly',
+                                alignItems:'center',
+                                height:'100%',
+                                border:'1px solid green'
+                                }}>
+                        <Link to='/nye-screenshot'><li style={{flexGrow:'1'}}>screenshot</li></Link>
+                        <Link to='/nye-scan'><li style={{flexGrow:'1'}}>scan</li></Link>
+
+                    </ul>
+
+            
+            </div>{/* .manager-page-wrapper */}
+        </>
+    )
+}
