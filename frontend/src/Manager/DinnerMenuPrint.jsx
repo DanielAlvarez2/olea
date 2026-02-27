2ef37339648b2e10400b0778d18b67f50225b241
Daniel Alvarez <daniel.yllanes@hotmail.com>
2026-02-27 02:54:30 -0500

    QR code in print dinner menu screen working

--- frontend/src/Manager/DinnerMenuPrint.jsx
+++ frontend/src/Manager/DinnerMenuPrint.jsx
@@ -470,7 +470,11 @@ Please switch to a different browser to proceed.
                                         
                                         
                                         <div style={{display:'flex',justifyContent:'space-between',alignItems:'center'}}>
+
                                             <div className='chef name' style={{textDecoration:'underline'}}>manuel romero, chef</div>
+                                            
+                                            <img src='qr-dinner.png' height='60px' />
+                                            
                                             <div style={{width:'65%'}}>
                                                 <span style={{fontWeight:'100'}}>
                                                     consumer advisory: consumption of undercooked meat, poultry, 
