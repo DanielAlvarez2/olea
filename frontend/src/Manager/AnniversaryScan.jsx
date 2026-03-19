b877dbe56467e032d94a4f5390f4babfae94a17c
Daniel Alvarez <daniel.yllanes@hotmail.com>
2026-03-19 17:08:32 -0400

    add scan: restaurant weeks fall

--- frontend/src/Manager/AnniversaryScan.jsx
+++ frontend/src/Manager/AnniversaryScan.jsx
@@ -1,7 +1,7 @@
 export default function AnniversaryScan(){
     return (
         <>
-            <img src='scan-anniversary.jpg' style={{width:'5.5in',height:'8.5in'}} />
+            <img src='scan-anniversary.jpg' style={{maxWidth:'100%'}} />
         </>
     )
 }
