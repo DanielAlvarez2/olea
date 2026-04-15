bfc7ec076883fbcc1201e5c588384dd77be992d5
Daniel Alvarez <daniel.yllanes@hotmail.com>
2026-04-15 14:53:24 -0400

    Specials.jsx pics displaying in modal

--- frontend/src/Dessert.jsx
+++ frontend/src/Dessert.jsx
@@ -4,6 +4,7 @@ import Navbar from './components/Navbar.jsx'
 import OpenTable from './components/OpenTable.jsx'
 import Footer from './components/Footer.jsx'
 import { useState,useEffect } from 'react'
+import { AiTwotoneCloseCircle } from "react-icons/ai";
 
 export default function Dessert(){
     useEffect(()=>window.scrollTo(0,0),[])
@@ -25,9 +26,60 @@ export default function Dessert(){
         }
     }
 
+    function showModal(pic,name,price,descriptionIntro,description){
+        if(pic == 'undefined') return
+        document.querySelector('.modal').style.display = 'grid'
+        document.querySelector('.modal-image').src = pic
+        document.querySelector('.modal-name').innerHTML = name
+        document.querySelector('.modal-price').innerHTML = price
+        if(descriptionIntro) document.querySelector('.modal-description-intro').innerHTML = `${descriptionIntro}; `
+        document.querySelector('.modal-description').innerHTML = description        
+    }
+
+    function closeModal(){
+        document.querySelector('.modal-image').src = ''
+        document.querySelector('.modal-name').innerHTML = ''
+        document.querySelector('.modal-price').innerHTML = ''
+        document.querySelector('.modal-description-intro').innerHTML = ''
+        document.querySelector('.modal-description').innerHTML = ''
+        document.querySelector('.modal').style.display = 'none'
+        document.querySelector('.modal').style.display = 'none'
+    }
+    
 
     return (
         <div className='page-wrapper webpage'>
+                    <div className='modal' style={{ position:'fixed',
+                                                    inset:'0',
+                                                    height:'100vh',
+                                                    width:'100%',
+                                                    zIndex:'1000',
+                                                    background:'#888888ee',
+                                                    color:'black',
+                                                    display:'none',
+                                                    placeContent:'center'
+                    }}>
+                        <AiTwotoneCloseCircle   size='70' 
+                                                onClick={closeModal}
+                                                style={{position:'fixed',
+                                                        cursor:'pointer',
+                                                        top:'5px',
+                                                        right:'5px'}} />
+                        <div className='modal-content'>
+                            <figure style={{display:'table'}}>
+                                <img className='modal-image' style={{maxHeight:'50vh',maxWidth:'90vw',borderRadius:'25px'}} />
+                                <figcaption style={{display:'table-caption',padding:'10px',captionSide:'bottom',borderRadius:'25px',background:'#ccc'}}>
+                                    <div style={{display:'flex',justifyContent:'space-between'}}>
+                                        <span className='modal-name' style={{fontWeight:'900'}}></span>
+                                        <span className='modal-price'></span>
+                                    </div>
+                                    <span className='modal-description-intro' style={{fontStyle:'italic'}}></span>
+                                    <span className='modal-description'></span>
+                                </figcaption>
+                            </figure>
+                        </div>{/* .modal-content */}
+                    </div>{/* .modal */}
+
             <div className='webpage-wrapper'>
                 <Navbar />
 
--- frontend/src/Specials.jsx
+++ frontend/src/Specials.jsx
@@ -4,6 +4,8 @@ import Navbar from './components/Navbar.jsx'
 import OpenTable from './components/OpenTable.jsx'
 import Footer from './components/Footer.jsx'
 import { useEffect, useState } from 'react'
+import { AiTwotoneCloseCircle } from "react-icons/ai";
+
 
 export default function Home(){
     const BASE_URL = (process.env.NODE_ENV == 'production') ?
@@ -19,8 +21,58 @@ export default function Home(){
             .then(data=>setAllSpecials(data))
             .catch(err=>console.log(err))
     }
+
+    function showModal(pic,name,price,description){
+        if(pic == 'undefined') return
+        document.querySelector('.modal').style.display = 'grid'
+        document.querySelector('.modal-image').src = pic
+        document.querySelector('.modal-name').innerHTML = name
+        document.querySelector('.modal-price').innerHTML = price
+        document.querySelector('.modal-description').innerHTML = description        
+    }
+
+    function closeModal(){
+        document.querySelector('.modal-image').src = ''
+        document.querySelector('.modal-name').innerHTML = ''
+        document.querySelector('.modal-price').innerHTML = ''
+        document.querySelector('.modal-description').innerHTML = ''
+        document.querySelector('.modal').style.display = 'none'
+        document.querySelector('.modal').style.display = 'none'
+    }
+
+
     return (
         <div className='page-wrapper webpage'>
+                    <div className='modal' style={{ position:'fixed',
+                                                    inset:'0',
+                                                    height:'100vh',
+                                                    width:'100%',
+                                                    zIndex:'1000',
+                                                    background:'#888888ee',
+                                                    color:'black',
+                                                    display:'none',
+                                                    placeContent:'center'
+                    }}>
+                        <AiTwotoneCloseCircle   size='70' 
+                                                onClick={closeModal}
+                                                style={{position:'fixed',
+                                                        cursor:'pointer',
+                                                        top:'5px',
+                                                        right:'5px'}} />
+                        <div className='modal-content'>
+                            <figure style={{display:'table'}}>
+                                <img className='modal-image' style={{maxHeight:'50vh',maxWidth:'90vw',borderRadius:'25px'}} />
+                                <figcaption style={{display:'table-caption',padding:'10px',captionSide:'bottom',borderRadius:'25px',background:'#ccc'}}>
+                                    <div style={{display:'flex',justifyContent:'space-between'}}>
+                                        <span className='modal-name' style={{fontWeight:'900'}}></span>
+                                        <span className='modal-price'></span>
+                                    </div>
+                                    <span className='modal-description'></span>
+                                </figcaption>
+                            </figure>
+                        </div>{/* .modal-content */}
+                    </div>{/* .modal */}
+
             <div className='webpage-wrapper'>
                 <Navbar />
 
@@ -43,7 +95,14 @@ export default function Home(){
                                     .filter(item=>item.section == 'appetizers' && item.sequence)
                                     .map(data=>{
                                         return(
-                                            <>  <div style={{display:'flex',justifyContent:'space-between'}}>
+                                            <div    key={data._id}
+                                                    onClick={()=>showModal( `${data.cloudinary_secure_URL}`,
+                                                                                            `${data.name}`,
+                                                                                            `${data.price}`,
+                                                                                            `${data.description}`                            
+                                                                                            )}                                            
+                                            >  
+                                                <div style={{display:'flex',justifyContent:'space-between'}}>
                                                     <span>
                                                         <span className='website-name'>{data.name}</span><br/>
                                                         {data.description}
@@ -51,7 +110,7 @@ export default function Home(){
                                                     <span>{data.price}</span>
                                                 </div>
                                                 <br/>
-                                            </>
+                                            </div>
                                         )
                                     })}
 
@@ -69,7 +128,14 @@ export default function Home(){
                                     .filter(item=>item.section == 'entrées' && item.sequence)
                                     .map(data=>{
                                         return(
-                                            <>  <div style={{display:'flex',justifyContent:'space-between'}}>
+                                            <div    key={data._id}
+                                                    onClick={()=>showModal( `${data.cloudinary_secure_URL}`,
+                                                                                            `${data.name}`,
+                                                                                            `${data.price}`,
+                                                                                            `${data.description}`                                        
+                                                                                            )}
+                                            >  
+                                                <div style={{display:'flex',justifyContent:'space-between'}}>
                                                     <span>
                                                         <span className='website-name'>{data.name}</span><br/>
                                                         {data.description}
@@ -77,7 +143,7 @@ export default function Home(){
                                                     <span>{data.price}</span>
                                                 </div>
                                                 <br/>
-                                            </>
+                                            </div>
                                         )
                                     })}
 
@@ -95,7 +161,14 @@ export default function Home(){
                                     .filter(item=>item.section == 'desserts' && item.sequence)
                                     .map(data=>{
                                         return(
-                                            <>  <div style={{display:'flex',justifyContent:'space-between'}}>
+                                            <div    key={data._id}
+                                                    onClick={()=>showModal( `${data.cloudinary_secure_URL}`,
+                                                                                            `${data.name}`,
+                                                                                            `${data.price}`,
+                                                                                            `${data.description}`                                        
+                                                                                            )}
+                                            >  
+                                                <div style={{display:'flex',justifyContent:'space-between'}}>
                                                     <span>
                                                         <span className='website-name'>{data.name}</span><br/>
                                                         {data.description}
@@ -103,7 +176,7 @@ export default function Home(){
                                                     <span>{data.price}</span>
                                                 </div>
                                                 <br/>
-                                            </>
+                                            </div>
                                         )
                                     })}
 
