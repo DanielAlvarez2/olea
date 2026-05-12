8742fd0b1135d6f8116ebfd17d9e40916b39fc59
Daniel Alvarez <daniel.yllanes@hotmail.com>
2026-05-12 07:43:53 -0400

    create model/schema for annual events

---
+++ backend/models/AnnualEvents.js
@@ -0,0 +1,15 @@
+const mongoose = require('mongoose')
+
+const AnnualEventsSchema = new mongoose.Schema({
+    ValentinesDay:{type,Boolean},
+    RestaurantWeekSpring:{type:Boolean},
+    MothersDay:{type:Boolean},
+    Commencement:{type:Boolean},
+    GraduationLunch:{type:Boolean},
+    OleaAnniversary:{type:Boolean},
+    ParentsWeekend:{type:Boolean},
+    RestaurantWeekFall:{type:Boolean},
+    NewYearsEve:{type:Boolean}
+},{timestamps:true})
+
+module.exports = mongoose.model('AnnualEvents',AnnualEventsSchema)
